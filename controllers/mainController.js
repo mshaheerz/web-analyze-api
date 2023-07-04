500bcdcdd1143f4c543fc54c16027c9c101d3ff7
mshaheerz <mshaheerkp9@gmail.com>
2023-07-04 16:15:18 +0530

    some fixes

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,137 +1,143 @@
 
 import userModel from "../models/userModel.js";
-import { compareSync, hashSync,genSaltSync } from "bcrypt";
+import { compareSync, hashSync, genSaltSync } from "bcrypt";
 const bcryptSalt = genSaltSync(10)
 import logModel from "../models/logsModel.js";
 import jwt from "jsonwebtoken";
 const jwtsecret = 'hehemysecret'
-import { getWebLinks,countWords,getMediaLinks } from "../helpers/mainHelper.js";
+import { getWebLinks, countWords, getMediaLinks } from "../helpers/mainHelper.js";
 
 
 // -------------------> user login <------------------------ //
-export async function login(req, res){
+export async function login(req, res) {
 
     try {
         const { email, password } = req.body;
         const user = await userModel.findOne({ email });
         if (user) {
-         console.log(req.body)
+            console.log(req.body)
             const passOk = compareSync(password, user.password)
             console.log(passOk)
             if (passOk) {
-                jwt.sign({id: user._id}, jwtsecret, { expiresIn: '1d' }, (err, token) => {
+                jwt.sign({ id: user._id }, jwtsecret, { expiresIn: '1d' }, (err, token) => {
                     if (err) throw err;
-                    res.cookie('token', token).json({status:'success', message:'user login success',user})
+                    res.cookie('token', token).json({ status: 'success', message: 'user login success', user })
                 })
 
             } else {
-                res.json({ 'status': 'failed', message:'Invalid password' })
+                res.json({ 'status': 'failed', message: 'Invalid password' })
             }
 
         } else {
-            res.json({status:'failed', message:'user not exist'})
+            res.json({ status: 'failed', message: 'user not exist' })
         }
     } catch (error) {
         console.log(error.message)
-        res.status(500).json({status:'failed', message:'internal server error'})
+        res.status(500).json({ status: 'failed', message: 'internal server error' })
     }
 
 }
 
 
 // -------------------> user register <------------------------ //
-export async function register(req, res){
+export async function register(req, res) {
     try {
         const { email, password } = req.body;
 
         const user = await userModel.create({
             email, password: hashSync(password, bcryptSalt)
         })
-        res.status(200).json({status:'success', message:'user successfully created',user});
+        res.status(200).json({ status: 'success', message: 'user successfully created', user });
     } catch (error) {
-        res.json({status:'failed', message:'failed to register'})
+        res.json({ status: 'failed', message: 'failed to register' })
     }
 }
 
 
 // -------------------> fetch user details <------------------------ //
-export async function profile(req,res){
+export async function profile(req, res) {
     try {
-        const {email, _id} = await userModel.findById(req.userId)
-        res.status(200).json({email,id:_id})
+        const { email, _id } = await userModel.findById(req.userId)
+        res.status(200).json({ email, id: _id })
     } catch (error) {
         console.log(error.message)
-        res.json({status:'failed', message:'internal server error'})
+        res.json({ status: 'failed', message: 'internal server error' })
     }
 }
 
 // -------------------> logout user <------------------------ //
 
-export function logout(req,res){
+export function logout(req, res) {
     res.cookie('token', '').json(true)
 }
 
 
-//
+//------------>main function to store all insights <---------------------//
 
-export async function getInsights(req, res){
+export async function getInsights(req, res) {
     try {
-        const {url}= req.body
+        const { url } = req.body
         const webLinks = await getWebLinks(url)
         const mediaLinks = await getMediaLinks(url)
         const wordCounts = await countWords(url)
-        const isLog = await logModel.findOne({domain:url})
-        if(isLog){
-            await logModel.findByIdAndUpdate(isLog._id,{
-                webLinks:webLinks,
-                mediaLinks:mediaLinks,
-                wordCount:wordCounts
+        const isLog = await logModel.findOne({ domain: url })
+        if (isLog) {
+            await logModel.findByIdAndUpdate(isLog._id, {
+                webLinks: webLinks,
+                mediaLinks: mediaLinks,
+                wordCount: wordCounts
             })
-        }else {
+        } else {
             await logModel.create({
-                user:req.userId,
-                domain:url,
-                webLinks:webLinks,
-                mediaLinks:mediaLinks,
-                wordCount:wordCounts
+                user: req.userId,
+                domain: url,
+                webLinks: webLinks,
+                mediaLinks: mediaLinks,
+                wordCount: wordCounts
             })
         }
 
-        const logs = await logModel.find({user:req.userId});
+        const logs = await logModel.find({ user: req.userId });
 
-        res.json({status:'success', logs})
+        res.json({ status: 'success', logs })
     } catch (error) {
         console.log(error.message)
     }
 }
 
-export async function getAllInsights(req, res){
+
+// finds all insights
+export async function getAllInsights(req, res) {
     try {
-        const logs = await logModel.find({user:req.userId}).sort({updatedAt:-1})
-        res.json({status:'success', logs})
+        const logs = await logModel.find({ user: req.userId }).sort({ updatedAt: -1 })
+        res.json({ status: 'success', logs })
     } catch (error) {
-        
+
     }
 }
 
+
+//updating insights
 export async function updateInsights(req, res) {
     try {
         const fav = req.body.fav
-        const id= req.params.id
-        await logModel.findOneAndUpdate({_id:id},{fav:fav})
-        return res.status(200).json({status:'success'})
+        const id = req.params.id
+        await logModel.findOneAndUpdate({ _id: id }, { fav: fav })
+        return res.status(200).json({ status: 'success' })
     } catch (error) {
-        
+
     }
-} 
+}
 
 
-export async function deleteInsights(req,res){
+
+//deleting insight
+export async function deleteInsights(req, res) {
     try {
-        const id= req.params.id
+        const id = req.params.id
         await logModel.findByIdAndDelete(id)
-        return res.status(200).json({status:'success'})
+        return res.status(200).json({ status: 'success' })
     } catch (error) {
-        
+
     }
 }
--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from 'cors';
 const router = express.Router();
 
 import { register,login,profile,logout,getInsights,getAllInsights,updateInsights, deleteInsights } from "../controllers/mainController.js";
@@ -9,7 +8,6 @@ router.post('/login',login)
 router.post('/register',register)
 router.get('/profile',userAuth,profile)
 router.post('/logout',logout)
-
 router.post('/insight',userAuth,getInsights)
 router.get('/insight',userAuth,getAllInsights)
 router.patch('/insight/:id',userAuth,updateInsights)
