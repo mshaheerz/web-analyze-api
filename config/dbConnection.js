e2582e4ab67c9130dfe735cd3d846cd3f00141de
shaheerez <72137242+mshaheerz@users.noreply.github.com>
2023-07-04 16:49:03 +0530

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -4,7 +4,7 @@ const db_url = process.env.DB_URL
 
 export async function connectDB(){
     try {
-     await mongoose.connect('mongodb://localhost:27017',{dbName:'webanalyze'})   
+     await mongoose.connect(db_url,{dbName:'webanalyze'})   
      console.log('db Connection successfull')
     } catch (error) {
       console.log(error.message)  
